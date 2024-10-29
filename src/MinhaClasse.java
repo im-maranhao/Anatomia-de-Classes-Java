cf69d33c712faa9ff569ea09b774b3f9a50707f1
Miriam Maranhão Da Silva <98363007+im-maranhao@users.noreply.github.com>
2024-10-28 22:07:06 -0300

    new modify

---
+++ src/MinhaClasse.java
@@ -0,0 +1,19 @@
+public class MinhaClasse{
+
+    public static void main(String [] args){
+        
+        // System.out.print("Olá Mundo");
+        String primeiroNome = "Miriam";
+        String segundoNome = "Maranhão";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do Método " + primeiroNome.concat(" ").concat(segundoNome);
+
+    }
+}
