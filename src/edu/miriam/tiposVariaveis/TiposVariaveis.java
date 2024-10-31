70433981295e4e9ee48ebd6db3d298cffabfbc28
Miriam Maranhão Da Silva <98363007+im-maranhao@users.noreply.github.com>
2024-10-30 23:22:33 -0300

    Atualizar o TiposVariaveis.java

--- src/edu/miriam/tiposVariaveis/TiposVariaveis.java
+++ src/edu/miriam/tiposVariaveis/TiposVariaveis.java
@@ -4,6 +4,8 @@ public class TiposVariaveis {
     public static void main(String[] args) {
         
         String meuNome = "Miriam Maranhao";
+        String sobre
+
 
         double salarioMinimo = 2500;
 
