8d87a19a56997b6066a4fb708300c04446887429
Miriam Maranhão Da Silva <98363007+im-maranhao@users.noreply.github.com>
2024-11-07 00:00:03 -0300

    new changed

--- src/edu/miriam/AnatomiaClasses/BoletimEstudantil.java
+++ src/edu/miriam/AnatomiaClasses/BoletimEstudantil.java
@@ -7,8 +7,10 @@ public class BoletimEstudantil {
 
         if (mediaFinal < 6)
             System.out.println("REPROVADO");
+
         else if (mediaFinal == 6)
             System.out.println("PROVA MINERVA");
+            
         else
             System.out.println("APROVADO");
 
